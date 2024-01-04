023a9bbd322ac4730baf1cb879e25e6f05650c5c
Bobby Coleman <bobbycoleman.dev@gmail.com>
2024-01-04 17:13:44 -0600

    connected stripe

---
+++ src/app/payments/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import PaymentForm from "./paymentsForm";
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+
+export default function PaymentsHome() {
+	return (
+		<Elements stripe={stripePromise}>
+			<PaymentForm />
+		</Elements>
+	);
+}
